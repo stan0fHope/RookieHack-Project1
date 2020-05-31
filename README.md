a155a6c326f84a221d1b6c4bc6ea7ec1d1ac1856
yugiohxl <38139912+yugiohxl@users.noreply.github.com>
2020-05-31 00:56:44 -0400

    Starting out late

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # RookieHack-Project1
 Solo Project for Hackathon, new language
 
-EIther in:
-Python w/ Flask or Java w/ React
+Writing a small game.
---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by stanh on 5/31/2020.
+//
+
+#include "main.h"
+
+int main(int argc, char *argv[]){
+    cout << "Welcome to my game"<< endl;
+}
